7998647cb78c1147821e3209daf52d2eaa55fe2f
Lizogubov Eugeny <noobikrus@gmail.com>
2024-04-14 16:42:19 +0500

    HW 2
    Realization of tasks 6-7

--- src/main/java/com/github/evgenylizogubov/HomeWorkApp.java
+++ src/main/java/com/github/evgenylizogubov/HomeWorkApp.java
@@ -3,12 +3,23 @@ package com.github.evgenylizogubov;
 import java.util.Arrays;
 
 public class HomeWorkApp {
+//    private static int[] arrayy = {1, 2, 3, 4, 5, 6, 7, 8};
+    
     public static void main(String[] args) {
         setValuesOfOneToArray();
         fillArray();
         doubleValuesOfArrayIfLessSix();
         fillTwoDimensionalArray();
         findMaxAndMinValuesInArray();
+        
+        int[] array1 = {2, 2, 2, 1, 2, 2, 10, 1};
+        System.out.println(checkEqualitySumsLeftAndRightPartsOfArray(array1));
+        System.out.println("---");
+        
+        int[] array2 = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(Arrays.toString(array2));
+        shiftArrayElements(array2, 3);
+        System.out.println(Arrays.toString(array2));
     }
     
     private static void setValuesOfOneToArray() {
@@ -87,5 +98,56 @@ public class HomeWorkApp {
         System.out.println("Min: " + min);
         System.out.println("Max: " + max);
         System.out.println("---");
+    }
+    
+    private static boolean checkEqualitySumsLeftAndRightPartsOfArray(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int leftPartSumArray = Arrays.stream(Arrays.copyOfRange(array, 0, i)).sum();
+            int rightPartSumArray = Arrays.stream(Arrays.copyOfRange(array, i, array.length)).sum();
+            
+            if (leftPartSumArray == rightPartSumArray) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private static void shiftArrayElements(int[] array, int shift) {
+        int n = array.length;
+        
+        if (shift > n) {
+            shift = shift % n;
+        }
+        
+        if (shift < 0) {
+            shift = n + shift;
+        }
+        
+        int i, j, k, temp;
+        for (i = 0; i < gcd(shift, n); i++) {
+            temp = array[i];
+            j = i;
+            while (true) {
+                k = j + shift;
+                if (k >= n) {
+                    k = k - n;
+                }
+                if (k == i) {
+                    break;
+                }
+                array[j] = array[k];
+                j = k;
+            }
+            array[j] = temp;
+        }
+    }
+    
+    // Function to get gcd of a and b
+    private static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
     }
 }
