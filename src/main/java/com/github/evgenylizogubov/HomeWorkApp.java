56ca6ef96d56931961c98edcb338f2ee1ddf5397
Lizogubov Eugeny <noobikrus@gmail.com>
2024-04-17 12:38:33 +0500

    HW 3
    realization task 2

--- src/main/java/com/github/evgenylizogubov/HomeWorkApp.java
+++ src/main/java/com/github/evgenylizogubov/HomeWorkApp.java
@@ -1,5 +1,6 @@
 package com.github.evgenylizogubov;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,10 +8,11 @@ public class HomeWorkApp {
     private static final Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
-        playGame();
+        guessTheNumber();
+        guessTheWord();
     }
     
-    private static void playGame() {
+    private static void guessTheNumber() {
         int hiddenNumber = new Random().nextInt(10);
         boolean win = false;
         
@@ -33,7 +35,7 @@ public class HomeWorkApp {
         System.out.println(win ? "Поздравляем! Вы отгадали загаданное число." : "Было загадано число - " + hiddenNumber);
         
         if (takeAndCheckInput("Повторить игру ещё раз? 1 - да / 0 - нет", 0, 1) == 1) {
-            playGame();
+            guessTheNumber();
         }
     }
     
@@ -46,5 +48,38 @@ public class HomeWorkApp {
         } while (input < minValue || input > maxValue);
         
         return input;
+    }
+    
+    private static void guessTheWord() {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
+        String hiddenWord = words[new Random().nextInt(words.length)];
+        char[] guessedLetters = "#".repeat(15).toCharArray();
+        boolean win = false;
+        String playerAnswer;
+        
+        System.out.println("Отгадайте загаданное слово");
+        
+        do {
+            System.out.println("Введите слово:");
+            playerAnswer = sc.nextLine().toLowerCase();
+            
+            if (playerAnswer.equals(hiddenWord)) {
+                win = true;
+            } else {
+                for (int i = 0; i < playerAnswer.length(); i++) {
+                    char letter = playerAnswer.charAt(i);
+                    
+                    if (hiddenWord.length() > i && letter == hiddenWord.charAt(i)) {
+                        guessedLetters[i] = letter;
+                    }
+                }
+                
+                System.out.println("Отгаданные буквы - " + Arrays.toString(guessedLetters));
+            }
+        } while (!win);
+        
+        System.out.println("Поздравляем! Вы отгадали!");
     }
 }
